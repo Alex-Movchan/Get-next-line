b74385d1839d333712d078e765fbbc6c14d56206
Alex Movchan <amovchan@e3r5p4.unit.ua>
2017-09-06 19:38:57 +0300

    read

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amovchan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/24 16:18:10 by amovchan          #+#    #+#             */
+/*   Updated: 2017/01/26 19:49:26 by amovchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 3
+
+int		get_next_line(const int fd, char **line);
+
+#endif
